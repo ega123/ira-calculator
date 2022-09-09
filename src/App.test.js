4b80ed28f2cefb55a6803cd115e5d4616239154c
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-09-09 19:23:46 +0100

    Add linting and testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('reaches correct result', async () => {
+  act(() => { // eslint-disable-line
+    render(<App />);
+  });
+  await new Promise((r) => setTimeout(r, 2000));
+  const linkElement = screen.getByText("The heat pump rebate for $10k in heat pump expenditures in 2023 is $8000.");
   expect(linkElement).toBeInTheDocument();
 });
