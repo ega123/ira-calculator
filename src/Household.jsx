86e5ad9eb841b9157a12d7e41c7dd93d67f87436
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-09-10 23:28:37 +0100

    Add API trigger

--- src/Household.jsx
+++ src/Household.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Radio } from "antd";
+import { Radio, Button } from "antd";
+import HouseholdData from './household';
 
 export default function Household(props){
     function handleChangeState(evt) {
@@ -10,28 +11,32 @@ export default function Household(props){
         props.setHousehold(updatedNewInfo);
       }
 
-    return(
+    return (
+        <>
         <form>
             <label>Income</label>
-            <input type="number" />
+            <input type="number" defaultValue={props.household.income} />
             <label>Marital Status</label>
-            <Radio.Group name="isMarried" value={props.household.answer} onChange={handleChangeState}>
-                <Radio.Button value="single">Single</Radio.Button>
-                <Radio.Button value="married">Married</Radio.Button>
+            <Radio.Group name="isMarried" defaultValue={props.household.married} onChange={handleChangeState}>
+                <Radio.Button value={false}>Single</Radio.Button>
+                <Radio.Button value={true}>Married</Radio.Button>
             </Radio.Group>
             <label>Number of dependents</label>
-            <Radio.Group name="haveChildren" value={props.household.answer} onChange={handleChangeState}>
-                <Radio.Button value="0">none</Radio.Button>
-                <Radio.Button value="1">1</Radio.Button>
-                <Radio.Button value="2">2</Radio.Button>
-                <Radio.Button value="3">3</Radio.Button>
-                <Radio.Button value="4">4</Radio.Button>
-                <Radio.Button value="5">5</Radio.Button>
+            <Radio.Group name="haveChildren" defaultValue={props.household.numChildren} onChange={handleChangeState}>
+                <Radio.Button value={0}>none</Radio.Button>
+                <Radio.Button value={1}>1</Radio.Button>
+                <Radio.Button value={2}>2</Radio.Button>
+                <Radio.Button value={3}>3</Radio.Button>
+                <Radio.Button value={4}>4</Radio.Button>
+                <Radio.Button value={5}>5</Radio.Button>
             </Radio.Group>
             <label>Zipcode</label>
-            <input type="number" />
-            
+            <input type="number" defaultValue={props.household.zipCode} />
         </form>
+        <Button onClick={() => {
+            new HouseholdData(props.household).calculateResults().then(results => alert(`Heat pump rebate: ${results.capped_heat_pump_rebate}`))
+        }}>Simulate</Button>
+        </>
     )
 
 }
