2fc673046e7f5da82b4fd3d099a492a1791ebce2
ega1231 <ega.munkhbat@gmail.com>
2022-09-10 14:02:33 -0700

    Added Expenditure data model

--- src/App.js
+++ src/App.js
@@ -13,17 +13,32 @@ class HouseholdInfo {
   }
 }
 
+class ExpenditureInfo{
+  constructor({new_clean_vehicle_classification,
+  new_clean_vehicle_msrp,
+  purchased_qualifying_new_clean_vehicle}){
+    this.new_clean_vehicle_classification = new_clean_vehicle_classification;
+    this.new_clean_vehicle_msrp = new_clean_vehicle_msrp;
+    this.purchased_qualifying_new_clean_vehicle = purchased_qualifying_new_clean_vehicle;
+  }
+}
 function App() {
+<<<<<<< HEAD
   // TODO: begin with a null household in the PR that creates Household component.
   // This is just placeholder data for now
+=======
+>>>>>>> Added Expenditure data model
   const [household, setHousehold] = useState(new HouseholdInfo({
     married: true,
     numChildren: 3,
     zipCode: 98102,
     income: 50000,
   }));
+<<<<<<< HEAD
   console.log(household, setHousehold);
 
+=======
+>>>>>>> Added Expenditure data model
   return (
     <div>
       <Header />
---
+++ src/Modal.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function Modal({formInfo}){
+    return(
+        <div>
+            
+        </div>
+    );
+}
